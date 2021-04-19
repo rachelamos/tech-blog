605dbfd779bce8ca9ba755c8a47fcde22a7b77da
Rachel Amos <rachelamos35@gmail.com>
2021-04-18 20:49:24 -0500

    get dashboard route working. all routes working.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,18 +51,27 @@ router.get('/post/:id', withAuth, async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/post', withAuth, async (req, res) => {
+router.get('/dashboard', async (req, res) => {
   try {
-    // Find the logged in user based on the session ID
-    const postData = await Post.findByPk(req.session.user_id, {
-      include: [{ model: Post }],
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+      where: {
+        user_id: req.session.user_id
+      }
     });
-
-    const user = postData.get({ plain: true });
-
-    res.render('post', {
-      ...user,
-      logged_in: true
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts)
+    // Pass serialized data and session flag into template
+    res.render('dashboard', { 
+      posts, 
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
